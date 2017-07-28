9e10430e9222e1c94b336a37bb6addd6769b96bf
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-28 16:00:03 +0800

    Updated test.

--- karv-test.js
+++ karv-test.js
@@ -1,10 +1,13 @@
 const assert = require( "assert" );
 const karv = require( "./karv.js" );
 
-assert.deepEqual( karv( { "name": "simple" } ), { "name": "simple" }, "should be deeply equal" );
+assert.deepEqual( karv( { "name": "simple" } ),
+	{ "name": "simple" }, "should be equal to { 'name': 'simple' }" );
 
-assert.equal( typeof karv( { "name": "simple" } ) == "object", true, "should return true" );
+assert.equal( typeof karv( { "name": "simple" } ) == "object",
+	true, "should be equal to true" );
 
-assert.deepEqual( karv( Object.freeze( { "name": "simple" } ) ), { "name": "simple" }, "should be deeply equal" );
+assert.deepEqual( karv( Object.freeze( { "name": "simple" } ) ),
+	{ "name": "simple" }, "should be equal to { 'name': 'simple' }" );
 
 console.log( "ok" );
